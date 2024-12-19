65b89a437a08d2dfb09c2b0a51f017f7abf33bf9
foods zhang <foodszhang@gmail.com>
2024-12-19 19:24:54 +0800

    add

--- src/network/mix.py
+++ src/network/mix.py
@@ -103,8 +103,8 @@ class DIF_Net(nn.Module):
             self.image_encoding = "unet3"
             self.image_encoder = UNet3Plus(mid_ch, fast_up=False, use_cgm=False)
         self.position_encoder = get_encoder(position_encoding)
-        # self.mlp = DensityNetwork_debug(mid_ch + 32)
         self.mlp = DensityNetwork_debug(mid_ch + 32)
+        # self.mlp = DensityNetwork_debug(mid_ch)
 
         if self.combine == "mlp":
             self.view_mixer = MLP([num_views, num_views // 2, 1])
@@ -192,7 +192,7 @@ class DIF_Net(nn.Module):
         q = q.permute(0, 2, 1)
         p_feats = torch.cat([p_feats, q], dim=1)
 
-        p_pred = self.point_classifier(p_feats)
+        # p_pred = self.point_classifier(p_feats)
         p_feats = p_feats.permute(0, 2, 1)
         p_pred = self.mlp(p_feats)
         p_pred = p_pred.permute(0, 2, 1)
