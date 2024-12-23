30beb2944a3481ad60867254b74cfa85a997afc7
foods zhang <foodszhang@gmail.com>
2024-12-23 10:35:52 +0800

    add

--- src/dataset/multi_dataset.py
+++ src/dataset/multi_dataset.py
@@ -375,7 +375,7 @@ class MultiTIGREDataset(Dataset):
                 coords = torch.tensor(coords, dtype=torch.float32, device=self.device)
                 cl.append(coords)
             coords = torch.stack(cl, dim=0)
-            print('123123123', coords.max(), coords.min()))
+            print("123123123", coords.max(), coords.min())
 
             return {
                 "projs": projections,
