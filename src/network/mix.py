78cc9fb5b4beb3bb321031218be7f62d1b621ea0
foods zhang <foodszhang@gmail.com>
2024-12-23 10:10:58 +0800

    add

--- src/network/mix.py
+++ src/network/mix.py
@@ -90,7 +90,7 @@ class DIF_Net(nn.Module):
         self,
         num_views,
         combine,
-        mid_ch=32,
+        mid_ch=64,
         image_encoding="unet",
         position_encoding="hashgrid",
     ):
