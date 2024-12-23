6d594060752fb9e57af6f99a64189f5d4037ca75
foods zhang <foodszhang@gmail.com>
2024-12-23 09:52:01 +0800

    add

--- src/network/mix.py
+++ src/network/mix.py
@@ -105,7 +105,8 @@ class DIF_Net(nn.Module):
         self.position_encoder = get_encoder(position_encoding)
         # self.mlp = DensityNetwork_debug(mid_ch + 32)
         # self.mlp = DensityNetwork_debug(mid_ch)
-        self.mlp = DensityNetwork_debug(mid_ch * num_views + 32)
+        # self.mlp = DensityNetwork_debug(mid_ch * num_views + 32)
+        self.mlp = DensityNetwork_debug(mid_ch * num_views)
 
         # if self.combine == "mlp":
         #    self.view_mixer = MLP([num_views, num_views // 2, 1])
@@ -190,11 +191,11 @@ class DIF_Net(nn.Module):
 
         # 3. point-wise classification
         # p_feats B, 128 , N
-        q = self.position_encoder(data["pts"], 0.2)  # B, N, 32
-        q = q.permute(0, 2, 1)
-        q = (q - q.min()) / (q.max() - q.min())
-        p_feats = (p_feats - p_feats.min()) / (p_feats.max() - p_feats.min())
-        p_feats = torch.cat([p_feats, q], dim=1)
+        # q = self.position_encoder(data["pts"], 0.2)  # B, N, 32
+        # q = q.permute(0, 2, 1)
+        # q = (q - q.min()) / (q.max() - q.min())
+        # p_feats = (p_feats - p_feats.min()) / (p_feats.max() - p_feats.min())
+        # p_feats = torch.cat([p_feats, q], dim=1)
 
         # p_pred = self.point_classifier(p_feats)
         p_feats = p_feats.permute(0, 2, 1)
