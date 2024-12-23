479a9a946dc88f4a1b97bb8fde1a83987f12fa87
foodszhang <foodszhang@gmail.com>
2024-12-23 16:14:34 +0800

    add

--- src/dataset/multi_dataset.py
+++ src/dataset/multi_dataset.py
@@ -383,7 +383,7 @@ class MultiTIGREDataset(Dataset):
             )
             tq = Q.reshape(-1, 3)
             q_coords = self.geo.project(tq, self.angles[0])
-            q_coords = torch.tensor(q_coords, dtype=torch.float32)
+            q_coords = torch.tensor(q_coords, dtype=torch.float32, device=self.device)
             print("3123123123", q_coords, q_coords.shape)
             q_r = index_2d(projections[0], q_coords)
             q_r = q_r.detach().cpu().numpy()
