5f6711ecc228a10ad46d2f413b005e0f9da544e7
foods zhang <foodszhang@gmail.com>
2024-12-25 10:23:28 +0800

    add

--- src/dataset/multi_dataset.py
+++ src/dataset/multi_dataset.py
@@ -213,7 +213,7 @@ class MultiTIGREDataset(Dataset):
         points = points.astype(float) / (256 - 1)
         points = points.reshape(3, -1)
         self.points = points.transpose(1, 0)  # N, 3
-        self.points = torch.tensor(self.points, dtype=torch.float32, device=device)
+        # self.points = torch.tensor(self.points, dtype=torch.float32, device=device)
         self.npoint = 30000
         self.angles = np.linspace(0, 180 / 180 * np.pi, self.n_views + 1)[:-1]
         self.blocks = np.load(self.cfg["blocks"])
@@ -390,7 +390,6 @@ class MultiTIGREDataset(Dataset):
             points, p_gt = self.sample_points(block_coords, block_values)
             # q = coord_to_dif_base(points)
             q = points
-            print("qqq123123123", q.shape, p_gt.shape)
             # values = index_3d(image, points)
             cl = []
             for other_proj_num in range(self.n_views):
