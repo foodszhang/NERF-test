be9a621a0602d4da2ac14478eec8de249603c4ea
foods zhang <foodszhang@gmail.com>
2024-12-25 15:12:48 +0800

    add

--- gen_info.py
+++ gen_info.py
@@ -2,17 +2,17 @@ import os
 import json
 import numpy as np
 
-data_path = "/autodl-fs/subset0/"
+data_path = "/root/autodl-fs/subset0/"
 # data_path = "./data"
 files = []
 for file in os.listdir(data_path):
     if file.endswith("mhd"):
         files.append(file)
 data = {"train": [], "eval": [], "test": []}
-data["train"] = files[:10]
+data["train"] = files[-3:]
 data["eval"] = files[-2:-1]
 data["test"] = [files[-1]]
-data["n_views"] = 10
+data["n_views"] = 5
 data["config.yaml"] = "./data/luna16/config.yml"
 data["image"] = "./data/luna16/image/{}.nii.gz"
 data["projections"] = "./data/luna16/projection/{}.pickle"
