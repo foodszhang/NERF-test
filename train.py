c36e7284ae872dadab3452b53ba140b8bab21e64
foods zhang <foodszhang@gmail.com>
2024-12-31 15:36:45 +0800

    add

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ import numpy as np
 from tqdm import tqdm
 import argparse
 import skimage as ski
+from src.dataset.multi_dataset import coord_to_dif_base
 
 
 def coord_to_dif(points):
@@ -35,7 +36,7 @@ def config_parser():
     # parser.add_argument("--config", default=f"./config/nerf/chest_50.yaml", help="configs file path")
     parser.add_argument(
         "--config",
-        default=f"./config/Lineformer/luna16_50_simple.yaml",
+        default=f"./config/Lineformer/luna16_50.yaml",
         help="configs file path",
     )
     parser.add_argument("--gpu_id", default="0", help="gpu to use")
@@ -138,114 +139,117 @@ class BasicTrainer(Trainer):
 
         # Evaluate density      渲染3D图像
         pts = self.eval_dset.voxels.reshape(-1, 3)
+        q = coord_to_dif_base(pts)
+        cl = []
+        for other_proj_num in range(self.eval_dset.n_views):
+            coords = self.eval_dset.geo.project(
+                q, self.eval_dset.angles[other_proj_num]
+            )
+            # coords -> (-1, 1)
+            coords = torch.tensor(
+                coords, dtype=torch.float32, device=self.eval_dset.device
+            )
+            cl.append(coords)
+        coords = torch.stack(cl, dim=0)
         raw = render.run_network_with_dif(
             pts,
-            self.eval_dst.projs,
-            data["proj_pts"],
+            self.eval_dset.projs,
+            coords,
             self.net,
             self.dif_net,
         )  # run_network 输出衰减系数μ
-        for index, data in enumerate(self.eval_dset):
-            # stx()
-            raw = render.run_network_with_dif(
-                data["pts"],
-                data["projs"],
-                data["proj_pts"],
-                self.net,
-                self.dif_net,
-            )  # run_network 输出衰减系数μ
-            image = self.eval_dset["image"]
-            image = image.reshape(256, 256, 256)
-            image_pred = image_pred.reshape(256, 256, 256)
-            # stx()
-            loss["ssim_3d"] += get_ssim_3d(image_pred, image)
+        image = self.eval_dset.image
+        image = raw.reshape(256, 256, 256)
+        image_pred = image_pred.reshape(256, 256, 256)
+        # stx()
+        loss["ssim_3d"] += get_ssim_3d(image_pred, image)
 
-            show_slice = 5
-            show_step = image.shape[-1] // show_slice
-            show_image = image[..., ::show_step]
-            show_image_pred = image_pred[..., ::show_step]
-            show = []
-            for i_show in range(show_slice):
-                show.append(
-                    torch.concat(
-                        [show_image[..., i_show], show_image_pred[..., i_show]], dim=0
-                    )
+        show_slice = 5
+        show_step = image.shape[-1] // show_slice
+        show_image = image[..., ::show_step]
+        show_image_pred = image_pred[..., ::show_step]
+        show = []
+        for i_show in range(show_slice):
+            show.append(
+                torch.concat(
+                    [show_image[..., i_show], show_image_pred[..., i_show]], dim=0
                 )
-            show_density = torch.concat(show, dim=1)
-
-            # cast_to_image -> 转成 numpy并多加一个维度
-            # self.writer.add_image(
-            #    "eval/density (row1: gt, row2: pred)",
-            #    cast_to_image(show_density),
-            #    global_step,
-            #    dataformats="HWC",
-            # )
-
-            proj_pred_origin_dir = osp.join(self.expdir, f"{index}_proj_pred_origin")
-            proj_gt_origin_dir = osp.join(self.expdir, f"{index}_proj_gt_origin")
-            proj_pred_dir = osp.join(self.expdir, f"{index}_proj_pred")
-            proj_gt_dir = osp.join(self.expdir, f"{index}_proj_gt")
-            # os.makedirs(eval_save_dir, exist_ok=True)
-            os.makedirs(proj_pred_origin_dir, exist_ok=True)
-            os.makedirs(proj_gt_origin_dir, exist_ok=True)
-            os.makedirs(proj_pred_dir, exist_ok=True)
-            os.makedirs(proj_gt_dir, exist_ok=True)
-
-            # for i in tqdm(range(N)):
-            #    """
-            #    cast_to_image 自带了归一化, 1 - 放在外边
-            #    """
-            #    iio.imwrite(
-            #        osp.join(proj_pred_origin_dir, f"proj_pred_{str(i)}.png"),
-            #        (cast_to_image(projs_pred[i]) * 255).astype(np.uint8),
-            #    )
-            #    iio.imwrite(
-            #        osp.join(proj_gt_origin_dir, f"proj_gt_{str(i)}.png"),
-            #        (cast_to_image(projs[i]) * 255).astype(np.uint8),
-            #    )
-            #    iio.imwrite(
-            #        osp.join(proj_pred_dir, f"proj_pred_{str(i)}.png"),
-            #        ((1 - cast_to_image(projs_pred[i])) * 255).astype(np.uint8),
-            #    )
-            #    iio.imwrite(
-            #        osp.join(proj_gt_dir, f"proj_gt_{str(i)}.png"),
-            #        ((1 - cast_to_image(1 - projs[i])) * 255).astype(np.uint8),
-            #    )
-
-            ## stx()
-            # for ls in loss.keys():
-            #    self.writer.add_scalar(f"eval/{ls}", loss[ls], global_step)
-
-            # Save
-            # 保存各种视图
-            eval_save_dir = osp.join(self.evaldir, f"epoch_{idx_epoch:05d}")
-            os.makedirs(eval_save_dir, exist_ok=True)
-            # np.save(
-            #    osp.join(eval_save_dir, f"{index}image_pred.npy"),
-            #    image_pred.cpu().detach().numpy(),
-            # )
-            # np.save(
-            #    osp.join(eval_save_dir, f"{index}image_gt.npy"),
-            #    image.cpu().detach().numpy(),
-            # )
-            output = np.clip(image_pred.cpu().detach().numpy(), 0, 1)
-            gt = np.clip(image.cpu().detach().numpy(), 0, 1)
-            output *= 255.0
-            gt *= 255.0
-            output = output.astype(np.uint8)
-            gt = gt.astype(np.uint8)
-            save_path = os.path.join(eval_save_dir, f"{index}.nii.gz")
-            gt_save_path = os.path.join(eval_save_dir, f"{index}_gt.nii.gz")
-            save_nifti(output, save_path)
-            save_nifti(gt, gt_save_path)
-
-            iio.imwrite(
-                osp.join(eval_save_dir, f"{index}slice_show_row1_gt_row2_pred.png"),
-                (cast_to_image(show_density) * 255).astype(np.uint8),
             )
-            with open(osp.join(eval_save_dir, "stats.txt"), "w") as f:
-                for key, value in loss.items():
-                    f.write("%s: %f\n" % (key, value))
+        show_density = torch.concat(show, dim=1)
+
+        # cast_to_image -> 转成 numpy并多加一个维度
+        # self.writer.add_image(
+        #    "eval/density (row1: gt, row2: pred)",
+        #    cast_to_image(show_density),
+        #    global_step,
+        #    dataformats="HWC",
+        # )
+
+        proj_pred_origin_dir = osp.join(self.expdir, f"{index}_proj_pred_origin")
+        proj_gt_origin_dir = osp.join(self.expdir, f"{index}_proj_gt_origin")
+        proj_pred_dir = osp.join(self.expdir, f"{index}_proj_pred")
+        proj_gt_dir = osp.join(self.expdir, f"{index}_proj_gt")
+        # os.makedirs(eval_save_dir, exist_ok=True)
+        os.makedirs(proj_pred_origin_dir, exist_ok=True)
+        os.makedirs(proj_gt_origin_dir, exist_ok=True)
+        os.makedirs(proj_pred_dir, exist_ok=True)
+        os.makedirs(proj_gt_dir, exist_ok=True)
+
+        # for i in tqdm(range(N)):
+        #    """
+        #    cast_to_image 自带了归一化, 1 - 放在外边
+        #    """
+        #    iio.imwrite(
+        #        osp.join(proj_pred_origin_dir, f"proj_pred_{str(i)}.png"),
+        #        (cast_to_image(projs_pred[i]) * 255).astype(np.uint8),
+        #    )
+        #    iio.imwrite(
+        #        osp.join(proj_gt_origin_dir, f"proj_gt_{str(i)}.png"),
+        #        (cast_to_image(projs[i]) * 255).astype(np.uint8),
+        #    )
+        #    iio.imwrite(
+        #        osp.join(proj_pred_dir, f"proj_pred_{str(i)}.png"),
+        #        ((1 - cast_to_image(projs_pred[i])) * 255).astype(np.uint8),
+        #    )
+        #    iio.imwrite(
+        #        osp.join(proj_gt_dir, f"proj_gt_{str(i)}.png"),
+        #        ((1 - cast_to_image(1 - projs[i])) * 255).astype(np.uint8),
+        #    )
+
+        ## stx()
+        # for ls in loss.keys():
+        #    self.writer.add_scalar(f"eval/{ls}", loss[ls], global_step)
+
+        # Save
+        # 保存各种视图
+        eval_save_dir = osp.join(self.evaldir, f"epoch_{idx_epoch:05d}")
+        os.makedirs(eval_save_dir, exist_ok=True)
+        # np.save(
+        #    osp.join(eval_save_dir, f"{index}image_pred.npy"),
+        #    image_pred.cpu().detach().numpy(),
+        # )
+        # np.save(
+        #    osp.join(eval_save_dir, f"{index}image_gt.npy"),
+        #    image.cpu().detach().numpy(),
+        # )
+        output = np.clip(image_pred.cpu().detach().numpy(), 0, 1)
+        gt = np.clip(image.cpu().detach().numpy(), 0, 1)
+        output *= 255.0
+        gt *= 255.0
+        output = output.astype(np.uint8)
+        gt = gt.astype(np.uint8)
+        save_path = os.path.join(eval_save_dir, f"{index}.nii.gz")
+        gt_save_path = os.path.join(eval_save_dir, f"{index}_gt.nii.gz")
+        save_nifti(output, save_path)
+        save_nifti(gt, gt_save_path)
+
+        iio.imwrite(
+            osp.join(eval_save_dir, f"{index}slice_show_row1_gt_row2_pred.png"),
+            (cast_to_image(show_density) * 255).astype(np.uint8),
+        )
+        with open(osp.join(eval_save_dir, "stats.txt"), "w") as f:
+            for key, value in loss.items():
+                f.write("%s: %f\n" % (key, value))
 
         loss["ssim_3d_avg"] = loss["ssim_3d"] / len(self.eval_dset)
         if loss["ssim_3d"] > self.best_ssim_3d:
@@ -253,7 +257,7 @@ class BasicTrainer(Trainer):
                 {
                     "epoch": idx_epoch,
                     "network": self.net.state_dict(),
-                    # "network_fine": self.net_fine.state_dict() if self.n_fine > 0 else None,
+                    # "network_fine": self.net_fine.state_dict() if self.n_fine > 0 else none,
                     "optimizer": self.optimizer.state_dict(),
                 },
                 self.ckpt_best_dir,
@@ -265,7 +269,7 @@ class BasicTrainer(Trainer):
 
             # stx()
 
-            # Logging
+            # logging
 
         return loss
 
