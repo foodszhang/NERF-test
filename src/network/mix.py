6ae075ce1041660e0f3550eb97e27f9f491b93d4
foods zhang <foodszhang@gmail.com>
2025-01-02 17:36:15 +0800

    add

--- src/network/mix.py
+++ src/network/mix.py
@@ -170,7 +170,7 @@ class DIF_Net(nn.Module):
                 pred_list.append(p_pred)
 
         pred = torch.cat(pred_list, dim=2)
-        if self.training:
+        if not self.training:
             p_feats = torch.cat(p_feats_list, dim=2)
         else:
             p_feats = None
