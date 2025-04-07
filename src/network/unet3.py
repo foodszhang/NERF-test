a45b4bee1d88c957f6b26052b14063c9be4760b2
foods zhang <foodszhang@gmail.com>
2025-04-07 16:29:54 +0800

    add

--- src/network/unet3.py
+++ src/network/unet3.py
@@ -216,6 +216,7 @@ class UNet3Plus(nn.Module):
             self.encoder.channels[1:],
             skip_ch=skip_ch,
             dropout=dropout,
+            fast_up=fast_up,
             am=am,
         )
         self.decoder.apply(weight_init)
